5d1f794c48239344f6e03edfa2570c8554ec7799
Sassi Mohamed Amine <sssmouhamedamine@gmail.com>
2025-08-16 22:07:45 +0100

    Revert "first commit"
    This reverts commit 073187776be543a3b469fd28ae4e5febcf1cd5d6.

--- GymApp/src/App.tsx
+++ GymApp/src/App.tsx
@@ -5,21 +5,13 @@ import AdminDashboard from "./Pages/AdminDashboard";
 import AddMember from "./Pages/AddMember";
 import GetMember from "./Pages/GetMember";
 import GetEndedMember from "./Pages/GetEndedSubscription";
-import useAdminCheck from "./Pages/AdminCheck";
-
 function App() {
-  const auth = useAdminCheck();
   return (
     <>
       <Routes>
         <Route path="/register" element={<Register />}></Route>
-        {!auth ? (
-          <Route path="/" element={<Login />}></Route>
-        ) : (
-          <Route path="/" element={<AdminDashboard />}></Route>
-        )}
         <Route path="/login" element={<Login />}></Route>
-        <Route path="/admin-dashboard" element={<AdminDashboard />}></Route>
+        <Route path="/" element={<AdminDashboard />}></Route>
         <Route path="/add_member" element={<AddMember />}></Route>
         <Route path="/get_member" element={<GetMember />}></Route>
         <Route path="/get_ended_member" element={<GetEndedMember />}></Route>
