fe58aa32714d2342e0faa54951c9014a814065ec
Sassi Mohamed Amine <sssmouhamedamine@gmail.com>
2025-09-01 16:41:18 +0100

    fix1

--- Server/DB/firebase.mjs
+++ Server/DB/firebase.mjs
@@ -1,6 +1,11 @@
 import admin from "firebase-admin";
-import serviceAccount from "./config/serviceAccountKey.json" assert { type: "json" };
+import fs from "fs";
+import path from "path";
 
+const serviceAccountPath = path.resolve(
+  "./Server/config/serviceAccountKey.json"
+);
+const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf8"));
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
