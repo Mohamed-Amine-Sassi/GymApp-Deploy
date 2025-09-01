9a1d911678849eb26a0a465186e63d741767b1af
Sassi Mohamed Amine <sssmouhamedamine@gmail.com>
2025-09-01 17:38:25 +0100

    fix4

--- Server/DB/firebase.mjs
+++ Server/DB/firebase.mjs
@@ -1,7 +1,8 @@
 import admin from "firebase-admin";
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
-
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_KEY.replace(/\\n/g, "\n")
+);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
